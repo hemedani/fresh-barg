da81b7d65f26e6d8377b1054cda6eeb264ac9a6d
Syd Amir <hemedani@gmail.com>
2024-06-09 19:41:54 +0330

    increase lesan version

--- libs/bc/dbs/deps.ts
+++ libs/bc/dbs/deps.ts
@@ -1,5 +1,5 @@
 // export * from "/Users/syd/work/lesan/lesan-1/mod.ts";
-export * from "https://deno.land/x/lesan@v0.1.7/mod.ts";
+export * from "https://deno.land/x/lesan@v0.1.9/mod.ts";
 // export * from '/home/erfan/Desktop/lesan/mod.ts';
 export * as jwt from "https://deno.land/x/djwt@v2.8/mod.ts";
 export * as Redis from "https://deno.land/x/redis@v0.29.3/mod.ts";
---
+++ libs/fr/states/mod.ts
@@ -0,0 +1,11 @@
+import { signal } from "@preact/signals";
+
+function createAppState() {
+  const todos = signal([]);
+
+  const completed = computed(() => {
+    return todos.value.filter(todo => todo.completed).length
+  });
+
+  return { todos, completed }
+}

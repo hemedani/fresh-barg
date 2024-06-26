c53b79c897965ded7c3ce77977c85a731444d226
rezahatampoor <rezahtms@gmail.com>
2024-06-26 12:44:40 +0330

    feat: Create UserCard Ui

---
+++ front/components/organisms/UserCard.tsx
@@ -0,0 +1,10 @@
+export const UserCard = () => {
+  return (
+    <div className="user-card">
+      <img src="/images/profile.jpeg" alt="user image" loading="lazy" />
+      <span className="user-name">john doe</span>
+      <span className="user-organ">tem org</span>
+      <span className="user-unit">tem unit</span>
+    </div>
+  );
+};

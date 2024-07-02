391d00b5d6ed74dd98f386671869573dc4574675
rezahatampoor <rezahtms@gmail.com>
2024-07-02 15:22:52 +0330

    fix:fix get User types

--- libs/fr/states/user/getUser.ts
+++ libs/fr/states/user/getUser.ts
@@ -1,8 +1,10 @@
 import { DeepPartial, ReqType, userSchema } from "../../../../back/declarations/selectInp.ts";
 import { bargApi } from "../../mod.ts";
 import { Signal } from "@preact/signals";
+export type getUserSet = ReqType["main"]["user"]["getUser"]["set"]
+export type setUserSet = DeepPartial<ReqType["main"]["user"]["getUser"]["get"]>
 
-export const getUser = async (user: Signal<DeepPartial<userSchema>>, set: ReqType["main"]["user"]["getUser"]["set"], get: ReqType["main"]["user"]["getUser"]["get"]) => {
+export const getUser = async (user: Signal<DeepPartial<userSchema>>, set: getUserSet, get: setUserSet) => {
   const getUser = await bargApi.send({
     model: "user",
     act: "getUser",

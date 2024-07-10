baf7b1bc45eb223ed890fdee1a97fc9c5254085e
Syd Amir <hemedani@gmail.com>
2024-07-10 12:55:52 +0330

    new type of state data

--- libs/fr/mod.ts
+++ libs/fr/mod.ts
@@ -1,5 +1,11 @@
-import { lesanApi } from "../../back/declarations/selectInp.ts";
+import { DeepPartial, lesanApi } from "../../back/declarations/selectInp.ts";
 
 export const bargApi = lesanApi({ URL: "http://localhost:1377/lesan" });
+
+export type BargStates<T> = {
+  data: DeepPartial<T>;
+  loader: boolean;
+  err: string | null;
+};
 
 export * from "./states/mod.ts";

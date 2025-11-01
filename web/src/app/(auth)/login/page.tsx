38a530b260933753606f5bb0dd731b439d906abe
Erfangh-80 <erfangh4871gholami@gmail.com>
2025-11-01 17:15:07 +0330

    :hammer: created page login - ( sendPhone - otpPhone)

---
+++ web/src/app/(auth)/login/page.tsx
@@ -0,0 +1,13 @@
+import { LoginPage } from "@/components/pages";
+import { Metadata } from "next";
+
+const Page = () => {
+  return <LoginPage />;
+};
+
+export default Page;
+
+export const metadata: Metadata = {
+  title: "ورود",
+  description: "صفحه ورود",
+};

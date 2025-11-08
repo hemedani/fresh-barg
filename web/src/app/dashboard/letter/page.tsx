0affd0aa1209b88243816a3edb523f3ea490de84
Erfangh-80 <erfangh4871gholami@gmail.com>
2025-11-08 11:55:37 +0330

    :hammer:   create page detail letter

--- web/src/app/dashboard/letter/page.tsx
+++ web/src/app/dashboard/letter/page.tsx
@@ -1,17 +1,19 @@
 import { AdvancedFilters, LettersList, LetterStats } from "@/components/organisms/letter";
+import { LetterDetailsPage } from "@/components/pages/letter/DetailLetterPage";
 
 export default function LettersPage() {
   return (
-    <div className="p-6 space-y-6">
-      <div className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold text-white">مدیریت نامه‌ها</h1>
-      </div>
+    // <div className="p-6 space-y-6">
+    //   <div className="flex justify-between items-center">
+    //     <h1 className="text-2xl font-bold text-white">مدیریت نامه‌ها</h1>
+    //   </div>
 
-      <LetterStats />
+    //   <LetterStats />
 
-      <AdvancedFilters />
+    //   <AdvancedFilters />
 
-      <LettersList />
-    </div>
+    //   <LettersList />
+    // </div>
+    <LetterDetailsPage />
   )
 }

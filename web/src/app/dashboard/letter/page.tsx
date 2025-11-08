08bddc274a7070a1e937885c9ce2407fb494c9d3
Erfangh-80 <erfangh4871gholami@gmail.com>
2025-11-08 12:51:47 +0330

    :hammer:  show data letters

--- web/src/app/dashboard/letter/page.tsx
+++ web/src/app/dashboard/letter/page.tsx
@@ -1,19 +1,17 @@
 import { AdvancedFilters, LettersList, LetterStats } from "@/components/organisms/letter";
-import { LetterDetailsPage } from "@/components/pages/letter/DetailLetterPage";
 
 export default function LettersPage() {
   return (
-    // <div className="p-6 space-y-6">
-    //   <div className="flex justify-between items-center">
-    //     <h1 className="text-2xl font-bold text-white">مدیریت نامه‌ها</h1>
-    //   </div>
+    <div className="p-6 space-y-6">
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-bold text-white">مدیریت نامه‌ها</h1>
+      </div>
 
-    //   <LetterStats />
+      <LetterStats />
 
-    //   <AdvancedFilters />
+      <AdvancedFilters />
 
-    //   <LettersList />
-    // </div>
-    <LetterDetailsPage />
+      <LettersList />
+    </div>
   )
 }

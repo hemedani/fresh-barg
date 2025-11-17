f8ce684f0c65d4f3c7342da3c9d3abe175ca307c
Erfangh-80 <erfangh4871gholami@gmail.com>
2025-11-17 16:31:38 +0330

    :hammer: create recived and sender letter page

---
+++ web/src/app/dashboard/letter/received/page.tsx
@@ -0,0 +1,41 @@
+import { getLetters } from '@/app/actions/letter/gets';
+import { LettersList, LetterStats } from '@/components/organisms/letter';
+import { cookies } from 'next/headers';
+
+const Page = async ({ }) => {
+    const userCookie = (await cookies()).get("user");
+    const user = userCookie ? JSON.parse(userCookie.value) : null;
+
+    const userPosition = user.position[0]
+    const response = await getLetters({
+        get: {
+            _id: 1,
+            number: 1,
+            subject: 1,
+            leed: 1,
+            tags: 1,
+            created_at: 1,
+            delivered: 1,
+            is_end: 1,
+            content: 1,
+        },
+        set: {
+            recieversId: userPosition._id,
+            positionId: userPosition._id
+        },
+    });
+
+    return (
+        <div className="p-6 space-y-6">
+            <div className="flex justify-between items-center">
+                <h1 className="text-2xl font-bold text-white">مدیریت نامه‌ها</h1>
+            </div>
+
+            <LetterStats />
+
+            <LettersList initialLetters={response.body} />
+        </div>
+    )
+}
+
+export default Page

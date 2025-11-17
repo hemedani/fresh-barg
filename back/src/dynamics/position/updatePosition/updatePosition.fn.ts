3e1b4068f9ba80decf116107c8cbb61c68104130
Syd Amir <hemedani@gmail.com>
2025-11-17 11:50:37 +0330

    fixed update position and add unit

--- back/src/dynamics/position/updatePosition/updatePosition.fn.ts
+++ back/src/dynamics/position/updatePosition/updatePosition.fn.ts
@@ -1,42 +1,30 @@
-import { ActFn, ObjectId } from "share/deps.ts";
+import { ActFn, Infer, object, ObjectId } from "share/deps.ts";
 import { position } from "../../../../mod.ts";
+import { positionPure } from "share/schemas/mod.ts";
 
 export const updatePositionFn: ActFn = async (body) => {
-	//  const context: MyContext = coreApp.contextFns
-	//  .getContextModel() as MyContext;
-
-	// const _id = context.position._id;
-
 	const {
 		set: {
 			_id,
 			name,
+			panel,
+			features,
 		},
 		get,
 	} = body.details;
 
+	const pureStruct = object(positionPure);
+	const updateObj: Partial<Infer<typeof pureStruct>> = {};
+
+	name && (updateObj.name = name);
+	panel && (updateObj.panel = panel);
+	features && (updateObj.features = features);
+
 	return await position.findOneAndUpdate({
 		filter: { _id: new ObjectId(_id) },
 		update: {
-			$set: {
-				name,
-			},
+			$set: updateObj,
 		},
 		projection: get,
 	});
-	// const foundedPosition = await position.findOne({ _id: new ObjectId(_id) });
-	// !foundedPosition && throwError("position not exist");
-
-	// // console.log("foundedPosition on update method", foundedPosition);
-
-	// const updatedPosition = await position.updateOne({ _id: new ObjectId(_id) }, { $set: {
-	//     name
-	// }
-	// });
-
-	// console.log("updatedPosition on update method", updatedPosition);
-
-	// return Object.keys(get).length != 0
-	//     ? await position.findOne({ _id: new ObjectId(_id) }, get)
-	//     : { _id: _id };
 };
